53bb6e66e052d13cd4cb0b1f4624c7956bb5c8a8
Adam Kerr <sirpoy@hotmail.com>
2021-11-22 16:15:20 +0100

    first seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,39 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+10.times do
+  user = User.create!(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    birthdate: Faker::Date.birthday(min_age: 18, max_age: 80),
+    gender: ['male', 'female'].sample,
+    plug_type: [1, 2].sample,
+    email: Faker::Internet.email,
+    password: "123456"
+  )
+  plug = Plug.create!(
+    plug_type: [1, 2].sample,
+    location: Faker::Addresss.full_address,
+    availability: [0, 1].sample,
+    description: Faker::Lorem.paragraph,
+    fixed_cost_per_15_min: rand(1..6),
+    user: user
+  )
+  booking = Booking.create!(
+    user: user,
+    status: [0, 1].sample,
+    duration: rand(1..10),
+    date: Faker::Date.between(from: '2020-03-17', to: '2021-11-11'),
+    plug: plug
+  )
+  [0, 0, 1].each do |number|
+    Review.create!(
+      rating: rand(1..5),
+      contents: Faker::Quote.famous_last_words,
+      review_type: number,
+      booking: booking
+    )
+  end
+end
