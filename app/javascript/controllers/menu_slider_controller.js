d66930d41d6f624ed41470dc64fa5042f8907db3
Benjamin Dohna <dohnabenjamin@gmail.com>
2021-11-23 17:55:29 +0100

    Added slide-menu

---
+++ app/javascript/controllers/menu_slider_controller.js
@@ -0,0 +1,12 @@
+import {Controller} from "stimulus"
+export default class extends Controller {
+  static targets = ["menu", "button", "menuText", "menuContent"]
+  toggleMenu() {
+    console.log("toggleMenu")
+    this.menuTarget.classList.toggle("show-menu")
+    this.buttonTarget.classList.toggle("change-button")
+    this.buttonTarget.classList.toggle("change-bars")
+    // this.menuTextTarget.classList.toggle("d-none")
+    this.menuContentTarget.classList.toggle("move")
+  }
+}
