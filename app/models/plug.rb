ff6da02600e9d8b36c2bb170ba55b1733110c2f5
Adam Kerr <sirpoy@hotmail.com>
2021-11-23 16:18:01 +0100

    mapbox implementation, popup, indexing

--- app/models/plug.rb
+++ app/models/plug.rb
@@ -1,9 +1,11 @@
 class Plug < ApplicationRecord
   belongs_to :user
   has_many :bookings, dependent: :destroy
+  geocoded_by :location
 
   validates :plug_type, presence: true, inclusion: { in: [1, 2] }
   validates :availability, presence: true
   validates :location, presence: true
   validates :fixed_cost_per_15_min, presence: true
+  after_validation :geocode
 end
