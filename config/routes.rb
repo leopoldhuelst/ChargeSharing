de65976128a2257d6975c258838c03cc77cbdcb4
Benjamin Dohna <dohnabenjamin@gmail.com>
2021-11-25 20:55:46 +0100

    moved dashboard page to users/edit page and iplemented a edit user function, also the link in the menu goes there now, obviously

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,12 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'plugs#index'
+  resources :users, only: %i[show edit update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/dashboard', to: 'dashboards#show', as: 'dashboard'
   get 'plugs/:id/bookings/approve', to: 'bookings#approve', as: 'bookings_approve'
   resources :plugs, only: [:index] do
-    resources :bookings, only: [:create, :show]
+    resources :bookings, only: %i[create show]
   end
   # get '/plugs', to: 'plugs#index'
   # root to: 'plugs#index'
