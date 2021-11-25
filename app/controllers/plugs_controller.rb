65f3db02feda6e6703d3da1030baeaf9de69f36e
Leopold Huelst <leopold.huelst@gmail.com>
2021-11-26 00:39:04 +0100

    added mapbox for show and changed icons depending on available or unavailable

--- app/controllers/plugs_controller.rb
+++ app/controllers/plugs_controller.rb
@@ -9,9 +9,12 @@ class PlugsController < ApplicationController
         lat: plug.latitude,
         lng: plug.longitude,
         info_window: render_to_string(partial: "info_window", locals: { plug: plug }),
-        image_url: helpers.asset_url('mapmarker.png')
+        active_image_url: helpers.asset_url('mapmarker.png'),
+        inactive_image_url: helpers.asset_url('icons8-marker-storm-50.png'),
+        availability: plug.availability
       }
     end
+    # raise
   end
 
   def new
