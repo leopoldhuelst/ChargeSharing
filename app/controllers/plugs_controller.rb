4eab361d4f4443604730fe477f051a69afbe4b9f
Adam Kerr <sirpoy@hotmail.com>
2021-11-26 12:22:00 +0100

    added unavailable marker

--- app/controllers/plugs_controller.rb
+++ app/controllers/plugs_controller.rb
@@ -10,7 +10,7 @@ class PlugsController < ApplicationController
         lng: plug.longitude,
         info_window: render_to_string(partial: "info_window", locals: { plug: plug }),
         active_image_url: helpers.asset_url('mapmarker.png'),
-        inactive_image_url: helpers.asset_url('icons8-marker-storm-50.png'),
+        inactive_image_url: helpers.asset_url('mapmarkerunavailable.png'),
         availability: plug.availability
       }
     end
