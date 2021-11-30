e3c08ec1e1e466f70d33c64e2c21c472c936ff44
Jonas Stannowski <jonas.stannowski@icloud.com>
2021-11-30 16:08:05 +0100

    Added Java Script to review page

---
+++ app/javascript/controllers/new_review_controller.js
@@ -0,0 +1,40 @@
+import { Controller } from "stimulus"
+export default class extends Controller {
+  static targets = ["duration", "tab", "form"]
+  static values = {
+    seconds: Number
+  }
+
+  connect() {
+    this.timer = 0
+    this.listener()
+    this.durationTarget.innerText = this.formatTime(this.secondsValue)
+  }
+
+  listener() {
+    document.addEventListener(("click"), (event) => {
+      this.formTarget.classList.toggle("d-table")
+      this.formTarget.classList.toggle("d-none")
+    })
+  }
+
+
+  formatTime(seconds) {
+    const hours = Math.floor(seconds / 3600)
+    const minutes = Math.floor((seconds % 3600) / 60)
+    seconds = seconds % 60
+    return (
+      String(hours).padStart(2, "0") +
+      ":" +
+      String(minutes).padStart(2, "0") +
+      ":" +
+      String(seconds).padStart(2, "0")
+    )
+  }
+
+  disconnect() {
+    if (this.timer) {
+      clearInterval(this.timer)
+    }
+  }
+}
